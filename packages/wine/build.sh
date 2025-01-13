6a610d9f920125992e96ec6e8745e2219b78e0dc
cjxyz <cjxyz776@gmail.com>
2025-01-13 11:11:51 -0500

    wine: Update git url and commit

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -4,8 +4,8 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=2ca04fd1e5566b063dc7c89bf3731e478595093c
+GIT_URL=https://github.com/cjxyz/wine
+GIT_COMMIT=31e33b2483d6e8d6072609f2d01fb0fa9815c8ae
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
