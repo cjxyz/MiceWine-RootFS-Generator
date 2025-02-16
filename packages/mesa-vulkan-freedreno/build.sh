0b051fc16b73684b58fa2f90d7c521ea12a260d5
cjxyz <cjxyz776@gmail.com>
2025-02-15 21:15:28 -0500

    mesa-vulkan-freedreno: Update build.sh

--- packages/mesa-vulkan-freedreno/build.sh
+++ packages/mesa-vulkan-freedreno/build.sh
@@ -1,4 +1,4 @@
-PKG_VER=25.0.0-devel-[gss]
+PKG_VER=25-latest-release
 PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Turnip Driver"
 VK_DRIVER_LIB="libvulkan_freedreno.so"
@@ -6,7 +6,7 @@ VK_DRIVER_LIB="libvulkan_freedreno.so"
 BLACKLIST_ARCHITECTURE=x86_64
 
 GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=365cd04375baeb8c607c1a49e85e9a364e193a08
+GIT_COMMIT=dfd2af5da11eca3678c0b34a24776114950fb6dc
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 CPPFLAGS="-D__USE_GNU"
-MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
+MESON_ARGS="-Dbuildtype=release -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
