7f289fee1b22a06be2108b7984488a707ad22b30
CJ <cjxyz776@gmail.com>
2025-02-18 09:55:44 -0500

    Update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -4,8 +4,8 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCHITECTURE=aarch64
 
-GIT_URL="https://github.com/cjxyz/wine -b proton_9.0"
-GIT_COMMIT=23ee238f6cc70d9daff1d4cdff30e489dbc19b53
+GIT_URL=https://github.com/KreitinnSoftware/wine
+GIT_COMMIT=f055b615d87c987bdb156fd6d29b4eff00cd8d85
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
