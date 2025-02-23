bbcd8e0162711c32212c225a0d80afdeec24d9ea
cjxyz <cjxyz776@gmail.com>
2025-02-23 14:47:29 -0500

    packages: Compile Pipetto-crypto wrapper only

--- packages/mesa-vulkan-wrapper/build.sh
+++ packages/mesa-vulkan-wrapper/build.sh
@@ -1,9 +1,9 @@
-PKG_VER=25.0.0-[gss]
+PKG_VER=25.0.0-[Pipetto-crypto]
 PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Android Wrapper"
 VK_DRIVER_LIB="libvulkan_wrapper.so"
 
-GIT_URL=https://github.com/xMeM/mesa
-GIT_COMMIT=d7a274ab77ecadb4b06175c38b726d24a6cb9210
+GIT_URL="https://gitlab.freedesktop.org/Pipetto-crypto/mesa -b wrapper"
+GIT_COMMIT=7c4e6cfd5fabff066a5d7bf999541c6aee9212dc
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
