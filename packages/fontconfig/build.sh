b3a3edd3de6c3ee0b2a0cd29af6fb6f71b579678
cjxyz <cjxyz776@gmail.com>
2025-03-09 00:12:38 -0500

    packages: Add fontconfig

---
+++ packages/fontconfig/build.sh
@@ -0,0 +1,5 @@
+PKG_VER=2.16.0
+SRC_URL=https://www.freedesktop.org/software/fontconfig/release/fontconfig-${PKG_VER}.tar.xz
+CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE --enable-libxml2=no --enable-iconv=no --disable-docs --with-default-fonts=/system/fonts --with-add-fonts=$PREFIX/share/fonts"
+LDFLAGS="-L$PREFIX/lib"
+CFLAGS="-I$PREFIX/include"
