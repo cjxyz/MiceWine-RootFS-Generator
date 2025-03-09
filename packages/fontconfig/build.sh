0f14cf7b6edf25c92fe372cd225ed8dc38403eb7
cjxyz <cjxyz776@gmail.com>
2025-03-09 01:46:01 -0500

    fontconfig: try fix (1)

--- packages/fontconfig/build.sh
+++ packages/fontconfig/build.sh
@@ -3,3 +3,4 @@ SRC_URL=https://www.freedesktop.org/software/fontconfig/release/fontconfig-${PKG
 CONFIGURE_ARGS="--host=$TOOLCHAIN_TRIPLE host_alias=$TOOLCHAIN_TRIPLE --enable-libxml2=no --enable-iconv=no --disable-docs --with-default-fonts=/system/fonts --with-add-fonts=$PREFIX/share/fonts"
 LDFLAGS="-L$PREFIX/lib"
 CFLAGS="-I$PREFIX/include"
+RUN_POST_APPLY_PATCH="libtool --finish $PREFIX/lib"
