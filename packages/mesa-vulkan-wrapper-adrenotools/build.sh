86a65a200dee25f81878026835a5ee979aeb0fde
cjxyz <cjxyz776@gmail.com>
2025-05-03 23:16:41 -0500

    mesa-vulkan-wrapper-adrenotools: Update

--- packages/mesa-vulkan-wrapper-adrenotools/build.sh
+++ packages/mesa-vulkan-wrapper-adrenotools/build.sh
@@ -6,6 +6,6 @@ VK_DRIVER_LIB="libvulkan_wrapper.so"
 BLACKLIST_ARCH=x86_64
 
 GIT_URL=https://github.com/cjxyz/mesa
-GIT_COMMIT=c0bacaa18d8bf2a16f33e579259ab3effb6108fe
+GIT_COMMIT=99045533f835eff7abed64704a9374c3cc786524
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -ladrenotools -llinkernsbypass"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
